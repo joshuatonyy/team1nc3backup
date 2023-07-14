68678f0ef3dae21b41221af049985e85c830ef60
Joshua Tony <64730665+joshuatonyy@users.noreply.github.com>
2023-07-14 17:30:06 +0700

    Initial Commit

---
+++ team1nc3/Config/Apps/team1nc3App.swift
@@ -0,0 +1,17 @@
+//
+//  team1nc3App.swift
+//  team1nc3
+//
+//  Created by Joshua on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct team1nc3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ team1nc3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  team1nc3
+//
+//  Created by Joshua on 14/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("NC3")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
